af2af8b8de4764ce415651cdd9057f88ace78969
dvicjon <devinvjones@gmail.com>
2021-01-16 10:59:04 -0500

    cleaned up js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
+//GLOBAL VARIABLES 
+
 var APIKey = "d766966c411b7f57a270ea89ff2f7bdc"; 
 var cityFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#cityname"); 
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
-var repoEl = document.createElement("div"); //create a new div element called repoEl 6.2.5
-repoEl.classList = "weather-container";
+var mainContainerEl = document.querySelector("#main-container");
+var citySearchTerm = document.querySelector("#city-search-term");
+var cityEl = document.createElement("div"); 
+cityEl.classList = "weather-container";
 var cityname = nameInputEl.value.trim();
 var clearEl = document.querySelector("#clear-history"); 
 var historyEl = document.querySelector("#history");
-let searchHistory = JSON.parse(localStorage.getItem("search")) || []; 
+let searchHistory = JSON.parse(localStorage.getItem("search")) || []; //add the search history to local storage OR an empty array 
 console.log(searchHistory); 
 var searchEl = document.querySelector("#search-button"); 
 
@@ -23,38 +25,16 @@ windEl.classList = "weather-info";
 var uvEl = document.querySelector("#uv-index"); 
 uvEl.classList = "weather-info"; 
 
-
-
-
-//function that executes upon a form submission browser event 6.2.4
-// var formSubmitHandler = function(event) {
-//     event.preventDefault(); 
-//     repoContainerEl = ''; 
-//     var cityname = nameInputEl.value.trim(); 
-//     if(cityname) { //checks value - if the cityname matches: 6.2.4 - if the city matches 
-//         getcityRepos(cityname); // run function with the selected cityname 6.2.4 - run function with city name ie. getcityRepos(ottawa) 
-//         searchHistory.push(cityname); 
-//         localStorage.setItem("search", JSON.stringify(searchHistory)); 
-//         renderSearchHistory(); 
-//         nameInputEl.value = ""; //clears the <input>element's value - clears the form - 6.2.4
-//     } else { //if the cityname does not match: 6.2.4
-//         alert("Please enter a city.");
-//     }
-//     console.log(event);
-// };
-
-
-//this function "fetches" the info (HTTP request) from OpenWeather API
-//OpenWeather replies with JSON data -- use this for weather server API
-var getcityRepos = function(cityName) {
-    //format the github api url - can enter any cityname in "city"
+//this function "fetches" the weather info (HTTP request) from OpenWeather API. OpenWeather replies with JSON data. 
+var getCityWeather = function(cityName) {
+    //format the OpenWeather api url - can enter any cityname in "city"
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + APIKey; 
-    //make a request to the URL - 6.2.5 edited - 6.2.6 edited (404 ERROR and network connectivity)
+    //make a request to the URL(404 ERROR and network connectivity)
     fetch(apiURL).then(function(response) {
         //request for data was successful 
-        if (response.ok) { //"ok" - when the HTTP request status code is something in the 200s - ok = true 404 error - 6.2.6
+        if (response.ok) { //"ok" - when the HTTP request status code is something in the 200s - ok = true 404 error
             response.json().then(function(data) {
-                displayRepos(data,cityName); //when the response data is converted to JSON, it will be sent from getcityRepos to displayRepos 
+                displayCities(data,cityName); //when the response data is converted to JSON, it will be sent from getCityWeather to displayCities 
                 getCityIndex(data); 
                 forecast(data); 
             });
@@ -62,13 +42,9 @@ var getcityRepos = function(cityName) {
             alert("Error: " + response.statusText); //statusText property - what the issue is 
         }
     })
-    // .catch(function(error) { //6.2.6 - catch is way of handling nextwork errors - if successful will get returned in the .then() method if request fails it will be sent to .catch() method 
-    //     //notice this .catch() getting chained onto the end of the .then() method
-    //     alert("Unable to connect to OpenWeather"); 
-    // }); 
 };
 
-//uv index 
+//this function "fetches" the UV index info (HTTP request) from OpenWeatherAPI. OpenWeather replies with JSON data
 var getCityIndex = function(weather,lat,lon) {
     var lat = weather.coord.lat; 
     var lon = weather.coord.lon; 
@@ -78,62 +54,58 @@ var getCityIndex = function(weather,lat,lon) {
             response.json().then(function(data) {
                 var uvi = data.value; 
                 uvEl.innerHTML = '<span class="badge badge-pill badge-light" id="uvi-badge" <i class="fas fa-sun"></i>' + " UV Index: " + uvi + "</span>";
-                repoEl.appendChild(uvEl);
-                if (uvi < 2) {
+                cityEl.appendChild(uvEl);
+                if (uvi < 2) { //apply green background colour if less than 2 
                     $("#uvi-badge").css("background-color", "green")
-                } else if (uvi < 5) {
+                } else if (uvi < 5) { //apply yellow background colour if less than 5 
                     $("#uvi-badge").css("background-color", "yellow")
-                } else if (uvi < 7) {
+                } else if (uvi < 7) { //apply orange background colour if less than 7
                     $("#uvi-badge").css("background-color", "orange")
-                } else {
+                } else { //apply red background colour if more than 7 
                     $("#uvi-badge").css("background-color", "red")
                 }
             });
-        }else {
+        }else {//alert the user if there was an error 
             alert("Error: " + response.statusText);
         }
     })
     .catch(function(error) { //6.2.6 - catch is way of handling nextwork errors - if successful will get returned in the .then() method if request fails it will be sent to .catch() method 
-        //notice this .catch() getting chained onto the end of the .then() method
         alert("Unable to connect to OpenWeather"); 
     }); 
 };
 
-//will accept both the array of the repo data(repos) and the term we searched(searchTerm) for as parameters - 6.2.5
-var displayRepos = function(weather, searchTerm) {
+//will accept both the array of the city data(weather) and the term we searched(searchTerm) for as parameters
+var displayCities = function(weather, searchTerm) {
     console.log (weather); 
-    //clear old city inputted content before displaying new content 6.2.5
-    //repoContainerEl.textContent = ""; //clears text from repoContainerEl 6.2.5
+    //clear old city inputted content before displaying new content
     //show current date 
     var currentDate = moment().format("(L)");
     //current weather icon 
     var currentIcon = weather.weather[0].icon; 
     var iconUrl = "http://openweathermap.org/img/wn/"+currentIcon +"@2x.png";
-    $(repoSearchTerm).html(searchTerm + currentDate + "<img src="+iconUrl+">")//ensures the page displays the cityname/search term/date and icon 
-        //taking each repository "repos[i]"" and writing some of it's data to the page (owner and login and name)
+    $(citySearchTerm).html(searchTerm + currentDate + "<img src="+iconUrl+">")//ensures the page displays the cityname/search term/date and icon 
         var currentTemp = "Current temperature: " + weather.main.temp + " &deg C"; 
         var feelsLike = "Feels like: " + weather.main.feels_like + " &deg C"; 
         var humidity = "Humidity: " + weather.main.humidity + "%";  
         var wind = "Wind speed: " + weather.wind.speed + " m/sec";
-        //create a conatiner for each repo 6.2.5 
-            //create a span element to hold currentTemp 
+            //add current temp info to element 
             currentTempEl.innerHTML = "<i class='fas fa-thermometer-empty'></i> " + currentTemp; 
-            repoEl.appendChild(currentTempEl); 
+            cityEl.appendChild(currentTempEl); 
 
-            //create a span element to hold feelsLike 
+            //add feels like info to element  
             feelsLikeEl.innerHTML = "<i class='far fa-grin-beam-sweat'></i> " + feelsLike;  
-            repoEl.appendChild(feelsLikeEl);
+            cityEl.appendChild(feelsLikeEl);
 
-            //create a span element to hold humidity 
+            //add humidty info to element 
             humidityEl.innerHTML = "<i class='fas fa-water'></i> " +  humidity; 
-            repoEl.appendChild(humidityEl);
+            cityEl.appendChild(humidityEl);
 
-            //create a span element to hold wind
+            //add wind info to element 
             windEl.innerHTML = "<i class='fas fa-wind'></i> " + wind; 
-            repoEl.appendChild(windEl);
+            cityEl.appendChild(windEl);
         
         //append container to the dom 6.2.5
-        repoContainerEl.appendChild(repoEl); //append repo to dom 6.2.5 - add div to container 
+        mainContainerEl.appendChild(cityEl); //append repo to dom 6.2.5 - add div to container 
 }; 
 
 var forecast = function(city) {
@@ -148,39 +120,42 @@ var forecast = function(city) {
                     forecastEls[i].innerHTML=""; 
                     var forecastIndex = i*8 + 4; 
                     var forecastDate = moment(response.list[forecastIndex].dt, "X").format("(L)"); 
-                    var forecastDateEl = document.createElement("p"); 
+                    var forecastDateEl = document.createElement("p"); //create p element to hold the date 
                     forecastDateEl.innerHTML = forecastDate; 
                     forecastEls[i].append(forecastDateEl); 
-                    var forecastWeatherEl = document.createElement("img")
+                    var forecastWeatherEl = document.createElement("img") //create img element to hold the icon
                     forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[forecastIndex].weather[0].icon + "@2x.png");
                     forecastWeatherEl.setAttribute("alt", response.list[forecastIndex].weather[0].description);
                     forecastEls[i].append(forecastWeatherEl);
-                    var forecastTempEl = document.createElement("p");
+                    var forecastTempEl = document.createElement("p"); //create p element to hold the temperature 
                     forecastTempEl.innerHTML = "Temperature: " + response.list[forecastIndex].main.temp + "&deg C";
                     forecastEls[i].append(forecastTempEl);
-                    var forecastHumidityEl = document.createElement("p");
+                    var forecastHumidityEl = document.createElement("p"); //create p element to hold the humidity 
                     forecastHumidityEl.innerHTML = "Humidity: " + response.list[forecastIndex].main.humidity + "%";
-                    forecastEls[i].append(forecastHumidityEl);              
+                    forecastEls[i].append(forecastHumidityEl); // add each forecast at each array to the page           
                 }
             })
         }
     })
 };  
 
-searchEl.addEventListener("click",function() {
+//when search button is clicked, find the value of the searchTerm, run the main function with that searchTerm as a parameter, push to the index, save to local storage and add to the search history
+searchEl.addEventListener("click",function() { 
     var searchTerm = nameInputEl.value;
-    getcityRepos(searchTerm); 
+    getCityWeather(searchTerm); 
     searchHistory.push(searchTerm);
     localStorage.setItem("search",JSON.stringify(searchHistory));
     renderSearchHistory(); 
 })
 
+//when the clear button is clicked, empty searchHistory array, remove the items from the search history 
 clearEl.addEventListener("click", function() {
     searchHistory = []; 
     renderSearchHistory(); 
     localStorage.removeItem("search", searchHistory);
 })
 
+//create a new input item for each history item, based on the searcHistory length 
 function renderSearchHistory() {
     historyEl.innerHTML = ""; 
     for (let i=0; i<searchHistory.length; i++){
@@ -190,16 +165,16 @@ function renderSearchHistory() {
         historyItem.setAttribute("class", "form-control d-block bg-white");
         historyItem.setAttribute("value", searchHistory[i]);
         historyItem.addEventListener("click", function() {
-            getcityRepos(historyItem.value); 
+            getCityWeather(historyItem.value); //when the history is clicked, run the main fuction with the item clicked as the parameter
         })
-        historyEl.append(historyItem);
+        historyEl.append(historyItem); //add history item to the rest 
         console.log(historyItem.value);
     }
 }
 
 renderSearchHistory();
 if (searchHistory.length > 0) {
-    getcityRepos(searchHistory[searchHistory.length -1]);
+    getCityWeather(searchHistory[searchHistory.length -1]);
 }
 
 
